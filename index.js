74b08806e5b4cbdd3b157976244da84981b1f3bb
NallaBhargavi25 <158247990+NallaBhargavi25@users.noreply.github.com>
2024-12-29 20:27:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ document.addEventListener("keydown",function(event){
 function audioplay(sound){
     var audio;
     switch(sound){
-        case "w":audio = new Audio('sounds/crash.mp3');
+        case "w":audio = new Audio('crash.mp3');
         break;
-        case "a":audio = new Audio('sounds/kick-bass.mp3');
+        case "a":audio = new Audio('kick-bass.mp3');
         break;
-        case "s":audio = new Audio('sounds/snare.mp3');
+        case "s":audio = new Audio('snare.mp3');
         break;
-        case "d":audio = new Audio('sounds/tom-1.mp3');
+        case "d":audio = new Audio('tom-1.mp3');
         break;
-        case "j":audio = new Audio('sounds/tom-2.mp3');
+        case "j":audio = new Audio('tom-2.mp3');
         break;
-        case "k":audio = new Audio('sounds/tom-3.mp3');
+        case "k":audio = new Audio('tom-3.mp3');
         break;
-        case "l":audio = new Audio('sounds/tom-4.mp3');
+        case "l":audio = new Audio('tom-4.mp3');
         break;
         default:alert("error");
         break;
